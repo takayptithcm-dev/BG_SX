2072c3435714309eca9f7449e26da08167414b33
takayptithcm-dev <takay.ptithcm@gmail.com>
2026-04-25 18:49:14 +0700

    Fix base path formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: ' / BG_SX /' ,
+    base: '/BG_SX/' ,
     server: {
       port: 3000,
       host: '0.0.0.0',
